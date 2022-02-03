d5fdc1ed22fef6d2076a06414bea3c74e2801ab0
Tijani Habeeb <79145763+habeeb10@users.noreply.github.com>
2022-02-03 01:39:15 +0100

    feat: implemented navigation

---
+++ src/common/navigator/navigation.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import Character from '../../screens/Character';
+import Episode from '../../screens/Episode';
+
+const Stack = createStackNavigator();
+
+function Navigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator headerMode={'none'}>
+        <Stack.Screen name="character" component={Character} />
+        <Stack.Screen name="episode" component={Episode} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default Navigator;
