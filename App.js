9afcbf3e3a5acb769745829bcb7e680d467b1e65
Tijani Habeeb <79145763+habeeb10@users.noreply.github.com>
2022-02-04 15:01:38 +0100

    refactor: app packages

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navigator from './src/common/navigator/navigation';
+import Navigator from './src/navigator/navigation';
 
 export default function App() {
   return <Navigator />;
